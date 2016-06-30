03245e0cfa2bfcb5d993372e45fe7ea90183fd9d
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-06-30 14:06:59 +0200

    Initialisation du projet Scala

---
+++ src/Collections.scala
@@ -0,0 +1,18 @@
+
+/**
+ * Correspond au chapitre 6 de la documentation Scala de O'Reilly.
+ */
+class Collections {
+  
+  /**
+   * Méthode principale.
+   */
+  def main(args: Array[String]): Unit = {
+    // Création d'une liste avec ::
+    val numbers = 1 :: 2 :: 3 :: Nil
+    
+    // Concaténation de deux listes
+    
+  }
+  
+}
